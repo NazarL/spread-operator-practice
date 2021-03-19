da6bc19dfc808d491bbe09678f90a38bb2d41c93
NazarL <nazar.lebedenko@gmail.com>
2021-03-18 22:02:28 -0400

    use spread operator to update an array

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [item, setItem] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setItem(newValue);
+    console.log(item);
+  }
+
+  function createItem() {
+    setItems((prevItems) => {
+      return [...prevItems, item];
+    });
+    setItem("");
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleChange} type="text" value={item} />
+        <button onClick={createItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {items.map((todoItem) => (
+            <li>{todoItem}</li>
+          ))}
         </ul>
       </div>
     </div>
